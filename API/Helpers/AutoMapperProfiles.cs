23c603e3222db1b0a7959c8c9079df41103fb452
Bhavin Singhvi <singhvibhavin@gmail.com>
2024-03-06 18:10:58 +0530

    End of section 12

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -15,6 +15,7 @@ namespace API.Helpers
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DataeOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUsers>();
+            CreateMap<RegisterDto, AppUsers>();
         }
     }
 }
